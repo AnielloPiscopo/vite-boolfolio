4c5c1bf5cd3a6352e0348a934a6587f9c64ea560
AnielloPiscopo <aniello.piscopo0707@gmail.com>
2023-03-03 14:17:42 +0100

    Created all the necessary Vue files for the header

---
+++ src/components/header/NavBar.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  name: "HeaderNavBar",
+};
+</script>
+
+<template>
+  <nav>
+    <ul>
+      <li>
+        <a>Home</a>
+        <a>Projects</a>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<style lang="scss"></style>
