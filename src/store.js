9b1b5ab476985b0a586b8e9834434c2e61ac89a0
AnielloPiscopo <aniello.piscopo0707@gmail.com>
2023-03-06 23:51:35 +0100

    Added the apiUrl and apiToken in the store

--- src/store.js
+++ src/store.js
@@ -1,6 +1,9 @@
 import { reactive } from "vue";
 
 export const store = reactive({
+  apiUrl: "http://127.0.0.1:8000/api/projects",
+  apiToken:
+    "LH8F6e4dED317Q6WrQVEPiTdk9kmfqjkbxOTazPP2BMSUjU1fl5ZkX3Ed9mQuoY5OUHIGYqcGzaREo0hLVJHymWtkhGauF5JCxu3",
   getImgPath(imgPath) {
     return new URL("./assets/img/" + imgPath + ".png", import.meta.url).href;
   },
