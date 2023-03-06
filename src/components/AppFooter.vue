3277fef385546a8b91a2004f262ff3c996905906
AnielloPiscopo <aniello.piscopo0707@gmail.com>
2023-03-06 22:48:07 +0100

    Created the footer

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  name: "AppFooter",
+};
+</script>
+
+<template>
+  <footer>
+    <p>
+      &#169; <span><em>Aniello Piscopo</em></span>
+    </p>
+  </footer>
+</template>
+
+<style lang="scss"></style>
