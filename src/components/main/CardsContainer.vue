77512e8a0cfc438403051b601b48956216497514
AnielloPiscopo <aniello.piscopo0707@gmail.com>
2023-03-08 00:53:18 +0100

    Created two components for the general cards

---
+++ src/components/main/CardsContainer.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  name: "ContactsContainer",
+
+  data() {
+    return {};
+  },
+};
+</script>
+
+<template>
+  <div class="my_cards-container"></div>
+</template>
+
+<style lang="scss"></style>
