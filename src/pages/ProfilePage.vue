2a212497d6cc43014ade0cc1ca33457e3048b092
AnielloPiscopo <aniello.piscopo0707@gmail.com>
2023-03-08 23:30:19 +0100

    Added the SectionHeader component to ProfilePage

--- src/pages/ProfilePage.vue
+++ src/pages/ProfilePage.vue
@@ -1,9 +1,29 @@
 <script>
+import SectionsHeader from "../components/main/SectionsHeader.vue";
+
 export default {
   name: "ProfilePage",
+
+  components: {
+    SectionsHeader,
+  },
+
+  data() {
+    return {
+      title: "Profilo",
+      subtitle: `Ciao sono uno studente Boolean`,
+      specialWords: ["Boolean"],
+    };
+  },
 };
 </script>
 
-<template></template>
+<template>
+  <SectionsHeader
+    :pageTitle="title"
+    :pageSubtitle="subtitle"
+    :specialWords="specialWords"
+  />
+</template>
 
 <style lang="scss"></style>
