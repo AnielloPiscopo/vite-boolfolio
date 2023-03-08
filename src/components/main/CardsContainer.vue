2f1b793fbe44269011d626cfd2e7b0cbec79a21b
AnielloPiscopo <aniello.piscopo0707@gmail.com>
2023-03-08 23:27:08 +0100

    Created a general structure for a system of cards by creating a component for the container and another for the single element

--- src/components/main/CardsContainer.vue
+++ src/components/main/CardsContainer.vue
@@ -1,15 +1,35 @@
 <script>
+import SingleCard from "./SingleCard.vue";
+
 export default {
   name: "ContactsContainer",
 
-  data() {
-    return {};
+  components: {
+    SingleCard,
+  },
+
+  props: {
+    objsList: {
+      type: Array,
+      required: false,
+    },
+
+    cardClass: {
+      type: String,
+      required: true,
+    },
   },
 };
 </script>
 
 <template>
-  <div class="my_cards-container"></div>
+  <div class="my_cards-container">
+    <SingleCard
+      v-for="obj in objsList"
+      :cardInfos="obj"
+      :cardClass="cardClass"
+    />
+  </div>
 </template>
 
 <style lang="scss"></style>
---
+++ src/components/main/SingleCard.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+  name: "SingleCard",
+
+  props: {
+    cardInfos: {
+      type: Object,
+      required: false,
+    },
+
+    cardClass: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<template>
+  <article class="my_card" :class="cardClass">
+    <font-awesome-icon :icon="cardInfos.mainIcon" v-if="cardInfos.mainIcon" />
+    <h3>{{ cardInfos.name }}</h3>
+    <span>{{ cardInfos.detail }}</span>
+  </article>
+</template>
+
+<style lang="scss"></style>
