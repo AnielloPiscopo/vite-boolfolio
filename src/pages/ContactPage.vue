3874399d3fb2317af220c368e04b2a0cc7cc6475
AnielloPiscopo <aniello.piscopo0707@gmail.com>
2023-03-08 23:31:03 +0100

    Added the content to the ContactPage

--- src/pages/ContactPage.vue
+++ src/pages/ContactPage.vue
@@ -1,11 +1,13 @@
 <script>
 import SectionsHeader from "../components/main/SectionsHeader.vue";
+import CardsContainer from "../components/main/CardsContainer.vue";
 
 export default {
   name: "ContactPage",
 
   components: {
     SectionsHeader,
+    CardsContainer,
   },
 
   data() {
@@ -13,6 +15,20 @@ export default {
       title: "Area Contatti",
       subtitle: `Rimaniamo in contatto`,
       specialWords: ["contatto"],
+      contactElementsClass: "my_contact-card",
+      contactInfos: [
+        {
+          mainIcon: ["fas", "fa-phone-square-alt"],
+          name: "Il mio numero",
+          detail: "+39 379 284 0262",
+        },
+
+        {
+          mainIcon: ["fas", "fa-envelope-square"],
+          name: "La mia email",
+          detail: "aniello.piscopo0707@gmail.com",
+        },
+      ],
     };
   },
 };
@@ -25,6 +41,13 @@ export default {
       :pageSubtitle="subtitle"
       :specialWords="specialWords"
     />
+
+    <div class="my_body">
+      <CardsContainer
+        :objsList="contactInfos"
+        :cardClass="contactElementsClass"
+      />
+    </div>
   </section>
 </template>
 
