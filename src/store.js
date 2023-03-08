2a9748b2811005007d3cc63c0d4fc89f06288307
AnielloPiscopo <aniello.piscopo0707@gmail.com>
2023-03-09 00:30:29 +0100

    Modified the api_token

--- src/store.js
+++ src/store.js
@@ -4,7 +4,7 @@ export const store = reactive({
   apiUrl: "http://127.0.0.1:8000/api/projects",
 
   apiToken:
-    "LH8F6e4dED317Q6WrQVEPiTdk9kmfqjkbxOTazPP2BMSUjU1fl5ZkX3Ed9mQuoY5OUHIGYqcGzaREo0hLVJHymWtkhGauF5JCxu3",
+    "rUP2SKAw6OdXkU8hkm3nhiTilBcAzk0Vxd8qZBHyPD25ECh3qzU1KAYWCNpkdpaHphWZR2kxtmsGTnivHq8zJcnYORZA4C8tUhJN",
 
   getImgPath(imgPath) {
     return new URL("./assets/img/" + imgPath + ".png", import.meta.url).href;
