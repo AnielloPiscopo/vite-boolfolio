a301522976657ccf44ae81c3ce25440a16111de7
AnielloPiscopo <aniello.piscopo0707@gmail.com>
2023-03-10 00:44:25 +0100

    Added general components for the view of lists

---
+++ src/components/main/ListsContainer.vue
@@ -0,0 +1,36 @@
+<script>
+import SingleList from "./SingleList.vue";
+
+export default {
+  name: "ListsContainer",
+
+  components: {
+    SingleList,
+  },
+
+  props: {
+    listsInfos: {
+      type: Array,
+      required: false,
+    },
+
+    listClass: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="my_lists-container">
+    <SingleList
+      v-for="(listInfos, index) in listsInfos"
+      :key="index"
+      :listInfos="listInfos"
+      :listClass="listClass"
+    />
+  </div>
+</template>
+
+<style lang="scss"></style>
---
+++ src/components/main/SingleList.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+  name: "SingleList",
+
+  props: {
+    cardInfos: {
+      type: Object,
+      required: false,
+    },
+
+    cardClass: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<template>
+  <ul>
+    <li></li>
+  </ul>
+</template>
+
+<style lang="scss"></style>
