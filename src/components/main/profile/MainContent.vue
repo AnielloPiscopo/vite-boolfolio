04cb87642f5db362e661d586545ef85166fdd215
AnielloPiscopo <aniello.piscopo0707@gmail.com>
2023-03-11 23:16:49 +0100

    Added in the ProfilePage the SingleList and MainContent component and added the content in this last

---
+++ src/components/main/profile/MainContent.vue
@@ -0,0 +1,59 @@
+<script>
+import { store } from "../../../store";
+
+export default {
+  name: "MainContent",
+
+  data() {
+    return {
+      store,
+      icons: [
+        {
+          link: "https://www.linkedin.com/in/aniello-piscopo-77ba04269/",
+          icon: ["fab", "fa-linkedin-in"],
+          message: "Linkedin",
+        },
+
+        {
+          link: "https://github.com/AnielloPiscopo",
+          icon: ["fab", "fa-github"],
+          message: "Github",
+        },
+
+        {
+          link: `mailto:${store.personalEmail}`,
+          icon: ["far", "fa-envelope"],
+          message: "email:",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<template>
+  <div class="my_main-content">
+    <div class="my_upper-part d-flex">
+      <img
+        :src="store.getImgPath('profileImg')"
+        alt=""
+        class="img-fluid text-center"
+      />
+      <div class="my_icons-container">
+        <a :href="icon.link" v-for="icon in icons" target="_blank"
+          ><font-awesome-icon :icon="icon.icon"
+        /></a>
+      </div>
+    </div>
+    <p class="my_lower-part">
+      Ciao,sono Aniello Piscopo,un ragazzo con buone skills informatiche e alte
+      abilità organizzative che recentemente ha sviluppato una passione per
+      l'informatica e a causa di ciò ho deciso di seguire la strada per
+      diventare programmatore . Adoro spendere tempo ad aggiustare e curare
+      piccoli dettagli. Questo è il mio sito web ufficiale dedicato a mostrare
+      chi sono e tutti i miei progetti futuri.
+    </p>
+  </div>
+</template>
+
+<style lang="scss"></style>
--- src/pages/ProfilePage.vue
+++ src/pages/ProfilePage.vue
@@ -1,11 +1,15 @@
 <script>
 import SectionsHeader from "../components/main/SectionsHeader.vue";
+import MainContent from "../components/main/profile/MainContent.vue";
+import SingleList from "../components/main/SingleList.vue";
 
 export default {
   name: "ProfilePage",
 
   components: {
     SectionsHeader,
+    MainContent,
+    SingleList,
   },
 
   data() {
@@ -13,17 +17,48 @@ export default {
       title: "Profilo",
       subtitle: `Ciao sono uno studente Boolean`,
       specialWords: ["Boolean"],
+      profileElementsClass: "my_profile-el",
+      profileDetails: [
+        {
+          name: "Nome",
+          value: "Aniello",
+        },
+
+        {
+          name: "Cognome",
+          value: "Piscopo",
+        },
+
+        {
+          name: "Compleanno",
+          value: "12/01/2003",
+        },
+
+        {
+          name: "Nazione",
+          value: "Italia",
+        },
+
+        {
+          name: "Città",
+          value: "Casoria",
+        },
+      ],
     };
   },
 };
 </script>
 
 <template>
-  <SectionsHeader
-    :title="title"
-    :subtitle="subtitle"
-    :specialWords="specialWords"
-  />
+  <section id="profile">
+    <SectionsHeader
+      :title="title"
+      :subtitle="subtitle"
+      :specialWords="specialWords"
+    />
+    <MainContent />
+    <SingleList :listClass="profileElementsClass" :listInfos="profileDetails" />
+  </section>
 </template>
 
 <style lang="scss"></style>
