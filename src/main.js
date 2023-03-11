b8583b64992a6680bd9f44bc3e44b52f3634406d
AnielloPiscopo <aniello.piscopo0707@gmail.com>
2023-03-11 23:11:00 +0100

    Imported the free-brands-svg-icons of FontAwesome

--- src/main.js
+++ src/main.js
@@ -11,8 +11,9 @@ import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 /* import specific icons */
 import { fas } from "@fortawesome/free-solid-svg-icons";
 import { far } from "@fortawesome/free-regular-svg-icons";
+import { fab } from "@fortawesome/free-brands-svg-icons";
 
-library.add(fas, far);
+library.add(fas, far, fab);
 
 createApp(App)
   .component("font-awesome-icon", FontAwesomeIcon)
